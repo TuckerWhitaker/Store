0341db685681c30f8ca4181cb00c0923ae9120dc
Tucker Whitaker <trw2005.50@gmail.com>
2022-11-02 14:43:29 -0500

    AdminAddItemPage

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -3,9 +3,25 @@ import "./AdminPage.css";
 function AdminPage() {
   return (
     <div className="AdminPage">
-      <input className="AdminInput"></input>
-      <input className="AdminInput"></input>
-      <input className="AdminInput"></input>
+      <div className="AdminInParent">
+        <label className="AdminLabel">Name: </label>
+        <input className="AdminInput"></input>
+      </div>
+      <div className="AdminInParent">
+        <label className="AdminLabel">Description: </label>
+        <textarea className="AdminInput" id="description"></textarea>
+      </div>
+      <div className="AdminInParent">
+        <label className="AdminLabel">Price: </label>
+        <input type="number" className="AdminInput"></input>
+      </div>
+      <div className="AdminInParent">
+        <div className="AdminOptionContainer"></div>
+        <input className="AdminInput"></input>
+        <button>Add Option</button>
+        <input className="AdminInput"></input>
+        <button>Add Value to Selected Option</button>
+      </div>
     </div>
   );
 }
