5891762840616b708683f238c10b38853c0872fc
Tucker Whitaker <trw2005.50@gmail.com>
2023-03-16 12:01:22 -0500

    AdminPage Rework1

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import "./App.css";
-import AdminPage from "./pages/AdminPage";
-import AdminOrder from "./pages/components/AdminOrder";
-import CartPage from "./pages/components/CartPage";
-import ContactPage from "./pages/components/ContactPage";
-import CreateItem from "./pages/components/CreateItem";
-import ItemPage from "./pages/components/ItemPage";
-import Main from "./pages/Main.js";
-import Navbar from "./pages/Navbar";
-
 import {
 	BrowserRouter as Router,
 	Route,
 	Routes,
 	useParams,
 } from "react-router-dom";
+import "./App.css";
+import AdminOrder from "./pages/components/AdminOrder";
+import AdminPage from "./pages/components/AdminPage";
+import CartPage from "./pages/components/CartPage";
+import ContactPage from "./pages/components/ContactPage";
+import CreateItem from "./pages/components/CreateItem";
+import ItemPage from "./pages/components/ItemPage";
+import Main from "./pages/Main.js";
+import Navbar from "./pages/Navbar";
 
 function App() {
 	return (
---
+++ client/src/pages/components/AdminPage.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./AdminPage.css";
+
+function AdminPage() {
+	const [ItemList, SetItemList] = useState([]);
+
+	const GetItemList = () => {
+		axios.post("http://localhost:3001/api/GetItems").then((response) => {
+			SetItemList(response.data);
+		});
+	};
+
+	useEffect(() => {
+		GetItemList();
+		console.log(ItemList);
+	}, []);
+
+	return (
+		<div className="AdminPage">
+			<table>
+				<tbody>
+					<tr>
+						<td>ID</td>
+						<td>Name</td>
+						<td>Price</td>
+						<td>Options</td>
+
+						<td>Images</td>
+						<td>LimitedTimeDate</td>
+					</tr>
+					{ItemList.map((item, index) => {
+						return (
+							<tr key={index}>
+								<td> {item.id}</td>
+								<td>{item.name}</td>
+								<td>{item.price}</td>
+								<td>
+									{item.options.map((info, index) => {
+										return <div key={"OptN" + index}>{info[0]}</div>;
+									})}
+								</td>
+
+								<td>
+									{item.imageNames.map((info, index) => {
+										return <div key={"Img" + index}>{info}</div>;
+									})}
+								</td>
+								<td>{item.endDate}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
+}
+
+export default AdminPage;
